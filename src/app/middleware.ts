1accce0af04fc25a1bba5e9e2849826e2290907e
Tirth Shah <heytirth@Mac.lan>
2025-03-11 15:16:30 +0530

    TS| Update middleware to handle subdomain

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -5,6 +5,8 @@ export function middleware(req: NextRequest) {
   const host = req.headers.get('host') || ''
   const subdomain = host.split('.')[0] // Extract subdomain
 
+  console.log(subdomain, host)
+
   const authUser = req.cookies.get('auth-user')?.value
 
   // If user is not authenticated, redirect to login
@@ -14,6 +16,7 @@ export function middleware(req: NextRequest) {
 
   // Check if the request is coming from a subdomain
   if (subdomain !== 'founderled' && subdomain !== 'www') {
+    console.log(req.url)
     return NextResponse.rewrite(new URL(`/dashboard`, req.url))
   }
 
