d4dad36daac8b10bd7ebeceb7b6e373b4b19bd75
Tirth Shah <heytirth@Mac.lan>
2025-03-13 17:23:07 +0530

    TS| Added api

--- src/app/[subdomain]/page.tsx
+++ src/app/[subdomain]/page.tsx
@@ -1,9 +1,42 @@
 'use client'
 import { useParams } from 'next/navigation'
+import { useState } from 'react'
 
 export default function LoginPage() {
   const params = useParams()
   const tenant = params.subdomain
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  // Example function to handle form submission
+  const handleSubmit = async () => {
+    e.preventDefault()
+
+    try {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/posts',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          // This is the body of the request, which includes the form data
+          body: JSON.stringify({
+            title: email,
+            body: password,
+            userId: 1,
+          }),
+        },
+      )
+
+      // Response from the API
+      const data = await response.json()
+      console.log('User saved successfully:', data)
+    } catch (error) {
+      console.error('Error saving user:', error)
+    }
+  }
 
   return (
     <div className='flex flex-col min-h-screen items-center justify-center bg-gray-50'>
@@ -11,7 +44,7 @@ export default function LoginPage() {
         <h1 className='text-2xl font-semibold text-center mb-4 text-black'>
           {tenant ? `Welcome to ${tenant}` : 'Welcome'}
         </h1>
-        <form className='space-y-6'>
+        <form className='space-y-6' onSubmit={handleSubmit}>
           <div>
             <label
               htmlFor='email'
@@ -24,6 +57,8 @@ export default function LoginPage() {
               id='email'
               className='mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
               required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div>
@@ -38,6 +73,8 @@ export default function LoginPage() {
               id='password'
               className='mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
               required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <button
